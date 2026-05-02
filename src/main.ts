2efd34c6d2f83fca995af1dd480fc5c4548fcdcc
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-05-02 20:15:56 +0700

    fix: update CORS configuration for frontend access

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia"
 import { folderController } from "./modules/explorer/interface/folder.controller"
+import cors from "@elysiajs/cors"
 
 new Elysia()
+  .use(cors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
+    methods: ['GET'],
+  }))
   .use(folderController)
   .listen(3000)
 
